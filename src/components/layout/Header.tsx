dc7793cd10b6f406f4f31eaed3b9c386438e1f7d
O-Vanessa-R <vanessa.rolland@oclock.school>
2025-10-14 21:54:42 +0200

    fix: fix to vercel

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import { getDictionary } from "../../lib/i18n/getDictionary";
 import HeaderClient from "./HeaderClient";
 
 export default async function Header({ locale }: { locale: string }) {
-  const dict = await getDictionary(locale, "navbar");
+  const dict = await getDictionary(locale as "fr" | "en", "navbar");
 
   return <HeaderClient dict={dict} locale={locale} />;
 }
